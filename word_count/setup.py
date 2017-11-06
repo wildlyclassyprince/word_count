f4ac03695c5f17d282b1ae2d1fbdc2b11dd90290
wildlyclassyprince <wildlyclassyprince@users.noreply.github.com>
2017-11-06 12:55:14 +0200

    Update setup.py

--- word_count/setup.py
+++ word_count/setup.py
@@ -4,5 +4,8 @@ setup(
     name='word_count',
     entry_points={
         'console_scripts':['word_count=word_count:main',],
-    }
+    },
+    author = 'wildlyclassyprince',
+    author_email = 'lihtumb@gmail.com',
+    url = 'https://github.com/wildyclassyprince/word_count'
 )
